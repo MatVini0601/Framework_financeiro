059c28529fb1d02469aa5374bbb3ae44f377335c
Matheus Vinicius Mota <64799886+MatVini0601@users.noreply.github.com>
2020-05-30 20:46:14 -0300

    Initial commit

---
+++ js/login.js
@@ -0,0 +1,55 @@
+let Cadastro = []
+Cadastro = JSON.parse(localStorage.getItem('CadastroCliente'))
+function Cadastra(){
+
+    let nome = document.getElementById('txtnome').value;
+    let email = document.getElementById('txtemail').value;
+    let senha = document.getElementById('txtsenha').value;
+    let telefone = document.getElementById('txttelefone').value;
+    let endereco = document.getElementById('txtendereco').value;
+
+    if(localStorage.getItem('CadastroCliente') == null){
+        localStorage.setItem('CadastroCliente', '[]')
+        Cadastro = JSON.parse(localStorage.getItem('CadastroCliente'))
+        let id = Cadastro.length;
+        var Cliente = {id: id++,nome,email,senha,telefone,endereco,status}
+        Cadastro.push(Cliente)
+        localStorage.setItem('CadastroCliente',JSON.stringify(Cadastro))
+    }else{
+        let id = Cadastro.length;
+        var Cliente = {id: id++,nome,email,senha,telefone,endereco,status}
+        Cadastro.push(Cliente)
+        localStorage.setItem('CadastroCliente',JSON.stringify(Cadastro))
+    }
+    Limpar()
+}
+
+function Logar(){
+    let email = document.getElementById('txtlemail').value;
+    let senha = document.getElementById('txtlsenha').value;
+
+    Cadastro = JSON.parse(localStorage.getItem('CadastroCliente'));
+    if(email == "" || senha == ""){
+        alert('Digite a porcaria do texto seu arrombado')
+    }
+   Cadastro.forEach(element => {
+        if(element.email == email){
+           if(element.senha == senha){
+            alert('bem vindo '+ element.nome)
+            window.location.replace('index.html')
+            localStorage.setItem('UsuarioLogado',element.email)
+            localStorage.setItem('NomeLogado',element.nome)
+           }else{
+            alert("Cadastro não encontrado")
+           }     
+        }
+   });
+
+}
+
+function Limpar(){
+    var array = document.getElementsByTagName("input");
+    for(Itens of array){
+        Itens.value = "";
+    }
+}
