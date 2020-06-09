c386193e870a54790bfb7c1f28c3e3026db3f3e3
Gabriel Henrique Mota <64985532+Gaburieru35@users.noreply.github.com>
2020-06-09 17:23:54 -0300

    New HTML
    Some other big changes

---
+++ js/categoria.js
@@ -0,0 +1,7 @@
+function openForm() {
+    document.getElementById("addCategorias").style.display = "block";
+  }
+
+  function closeForma(){
+    document.getElementById("addCategorias").style.display = "none";
+  }
