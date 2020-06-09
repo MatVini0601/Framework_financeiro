fc29a26005f4f3ebd3d2beacaf3a5bb8136db012
Matheus Vinicius Mota <64799886+MatVini0601@users.noreply.github.com>
2020-06-09 11:08:17 -0300

    Alterar Perfil added
    the function alterarPerfil is now online

---
+++ js/perfil.js
@@ -0,0 +1,33 @@
+function verPerfil(){
+    let Cadastro = JSON.parse(localStorage.getItem('CadastroCliente'));
+    let id = JSON.parse(localStorage.getItem('IdAtual'))
+     console.log(Cadastro[id].nome) 
+
+    if(id == null){
+        alert('faça login primeiro')
+    }else{
+       document.getElementById('lblnome').innerHTML = Cadastro[id].nome
+       document.getElementById('lblemail').innerHTML = Cadastro[id].email
+       document.getElementById('lbltelefone').innerHTML = Cadastro[id].telefone
+       document.getElementById('lblendereco').innerHTML = Cadastro[id].endereco
+       document.getElementById('lblstatus').innerHTML = Cadastro[id].status
+    }
+}
+
+function alterarPerfil(){
+    let Cadastro = JSON.parse(localStorage.getItem('CadastroCliente'));
+    let id = JSON.parse(localStorage.getItem('IdAtual'))
+
+    if(document.getElementById('txtnome') == "" || document.getElementById('txtemail') == "" || document.getElementById('txttelefone') == "" || document.getElementById('txtendereco') == ""){
+        alert('Digite todos os campos')
+    }else{
+        Cadastro[id].nome = document.getElementById('txtnome').value
+        Cadastro[id].email = document.getElementById('txtemail').value
+        Cadastro[id].telefone = document.getElementById('txttelefone').value
+        Cadastro[id].endereco = document.getElementById('txtendereco').value
+
+        localStorage.setItem('CadastroCliente',JSON.stringify(Cadastro))
+        localStorage.setItem('UsuarioLogado',Cadastro[id].email)
+        localStorage.setItem('NomeLogado',Cadastro[id].nome)
+    }
+}
