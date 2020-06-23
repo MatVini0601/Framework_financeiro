faa2d027152cc7bb012661ca794dc5bc547cf390
Matheus Vinicius Mota <64799886+MatVini0601@users.noreply.github.com>
2020-06-23 13:35:16 -0300

    the last update

--- js/administrativo.js
+++ js/administrativo.js
@@ -1,9 +1,15 @@
 let receita = 0
 let despesa = 0
+let qreceita = 0
+let qdespesa = 0
 let i = 0   
 
 function openForm() {
     document.getElementById("addContas").style.display = "block";
+  }
+
+  function FormEditConta() {
+    document.getElementById("addContasEdit").style.display = "block";
   }
 
   function closeForm(){
@@ -14,15 +20,23 @@ function openForm() {
     let Contas = JSON.parse(localStorage.getItem('Contas'))
     let linhaCo = "";
     Contas.forEach(element => {
-      document.getElementById('txtcontaid').value = element.id
       let row = document.getElementById("txtcontanome");
         linhaCo += "<option value="+element.id+">"+element.titulo+"</option>"
         row.innerHTML = linhaCo;
        });
   }
 
+  function ListarEditContas(){
+    let Contas = JSON.parse(localStorage.getItem('Contas'))
+    let linhaCo = "";
+    Contas.forEach(element => {
+      let row = document.getElementById("txteditcontanome");
+        linhaCo += "<option value="+element.id+">"+element.titulo+"</option>"
+        row.innerHTML = linhaCo;
+       });
+  }
+
   function lancarContas(){
-    debugger
     let Contas = JSON.parse(localStorage.getItem('ContasLancadas'))
 
     if(localStorage.getItem('ContasLancadas') == null){
@@ -36,9 +50,9 @@ function openForm() {
       let data = now.getDate()+"/"+now.getMonth()+"/"+now.getFullYear()
       let hora = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()
 
-      dados = localizarConta(Contaid)
+      dados = localizarLancConta(Contaid)
                 var LancamentoConta = {
-                    id: id++ ,nome:dados.titulo ,categoria:dados.categoria.nome,tipo:dados.tipo,valor,data,hora
+                    id: id++ ,contarelacionada:dados,valor,data,hora
                 }
                 Contas.push(LancamentoConta)
                 localStorage.setItem('ContasLancadas',JSON.stringify(Contas))
@@ -50,9 +64,9 @@ function openForm() {
       let data = now.getDate()+"/"+now.getMonth()+"/"+now.getFullYear()
       let hora = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()
 
-      dados = localizarConta(Contaid)
+      dados = localizarLancConta(Contaid)
                 var LancamentoConta = {
-                    id: id++ ,nome:dados.titulo ,categoria:dados.categoria.nome,tipo:dados.tipo,valor,data,hora
+                  id: id++ ,contarelacionada:dados,valor,data,hora
                 }
                 Contas.push(LancamentoConta)
                 localStorage.setItem('ContasLancadas',JSON.stringify(Contas))
@@ -73,14 +87,14 @@ function openForm() {
     let linhan = ""
     ContasLancadas.forEach(element => {
       let row = document.getElementById("nome");
-      linhan += "<label>"+element.nome+"</label><br>"
+      linhan += "<label>"+element.contarelacionada.titulo+"</label><br>"
       row.innerHTML = linhan;  
       });
 
       let linhav = ""
     ContasLancadas.forEach(element => {
       let row = document.getElementById("valor");
-      if(element.tipo == "Receita"){
+      if(element.contarelacionada.tipo == "Receita"){
         linhav += "<label style='color: #13a100'>"+element.valor+"</label><br>"
         row.innerHTML = linhav; 
       }else{
@@ -106,13 +120,13 @@ function openForm() {
         let linhaedit = ""
         ContasLancadas.forEach(element => {
           let row = document.getElementById("edit");
-          linhaedit += "<button class='btn' onclick='Editar("+element.id+")'>Editar</button><br>"
+          linhaedit += "<button class='btn' onclick='editarConta("+element.id+")'>Editar</button><br>"
           row.innerHTML = linhaedit;  
           });
-debugger
+
           i = 0
           while(i < ContasLancadas.length){
-            if(ContasLancadas[i].tipo === "Receita"){
+            if(ContasLancadas[i].contarelacionada.tipo === "Receita"){
               receita += parseInt(ContasLancadas[i].valor)
               i++
               document.getElementById('labelavalor').innerHTML = receita
@@ -129,7 +143,54 @@ debugger
         }else{
           document.getElementById('labelasaldo').style.color = '#13a100'
         }
-        
+  }
+
+  function editarConta(id){
+    debugger
+    FormEditConta()
+    let Contas = JSON.parse(localStorage.getItem('Contas'))
+    let conta = localizarEditConta(id)
+
+    document.getElementById('txtcontaid').value = conta.id
+
+    let linhacontaedit = "";
+     Contas.forEach(element => {
+          let row = document.getElementById("txteditcontanome");
+          if(element.id === conta.contarelacionada.id){
+            linhacontaedit += "<option id="+conta.contarelacionada.titulo+" value="+conta.contarelacionada.id+" selected>"+conta.contarelacionada.titulo+"</option>"
+          }else{
+            linhacontaedit += "<option value="+element.id+">"+element.titulo+"</option>"
+          }
+          row.innerHTML = linhacontaedit;
+      })
+    document.getElementById('txteditvalor').value = conta.valor
+  }
+
+  function SalvarConta(){
+    let ContasLancadas = JSON.parse(localStorage.getItem('ContasLancadas'))
+    let id = document.getElementById('txtcontaid').value
+    let index = localizarIndiceConta(id)
+
+    let c = confirm('Tem certeza desta ação?')
+
+    if(c == true){
+      debugger
+      ContasLancadas.forEach(element => {
+        if(ContasLancadas[index].id == element.id){
+          var sel = document.getElementById("txteditcontanome");
+          var text = sel.options[sel.selectedIndex].value;
+          let indice = localizarConta(text)
+          element.contarelacionada = indice;
+          element.valor = document.getElementById('txteditvalor').value
+          let now = new Date
+          element.data = now.getDate()+"/"+now.getMonth()+"/"+now.getFullYear()
+          element.hora = now.getHours()+":"+now.getMinutes()+":"+now.getSeconds()
+          localStorage.setItem('ContasLancadas',JSON.stringify(ContasLancadas))
+        }
+      });
+    }else{
+      alert('Operação cancelada')
+    }
   }
 
   function localizarConta(id){
@@ -138,10 +199,37 @@ debugger
 
     return ContasLancadas[index]
   }
-  
-  function localizarEditConta(id){
+  function localizarLancConta(id){
     let ContasLancadas = JSON.parse(localStorage.getItem('Contas'))
-    let index = ContasLancadas.findIndex(categoria => categoria.id == id)
+    let index = ContasLancadas.findIndex(conta => conta.id == id)
+
+    return ContasLancadas[index]
+  }
+
+  function localizarIndiceConta(id){
+    let ContasLancadas = JSON.parse(localStorage.getItem('ContasLancadas'))
+    let index = ContasLancadas.findIndex(conta => conta.id == id)
 
     return index
+  }
+  
+  function localizarEditConta(id){
+    let Contas = JSON.parse(localStorage.getItem('ContasLancadas'))
+    let index = Contas.findIndex(conta => conta.id == id)
+
+    return Contas[index]
+  }
+
+  function quantidade(){
+    let ContasLancadas = JSON.parse(localStorage.getItem('ContasLancadas'))
+
+    ContasLancadas.forEach(element => {
+      if(element.contarelacionada.tipo == "Receita"){
+        qreceita++
+      }else{
+        qdespesa++
+      }
+      document.getElementById('qdespesa').innerHTML = qdespesa
+      document.getElementById('qreceita').innerHTML = qreceita
+    });
   }
