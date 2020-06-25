7fe29c9f5451376e7aad126b8cdfd3e45477081f
Matheus Vinicius Mota <64799886+MatVini0601@users.noreply.github.com>
2020-06-25 18:17:12 -0300

    Update contas.js

--- js/contas.js
+++ js/contas.js
@@ -11,17 +11,15 @@ function openForm() {
   }
 
 function lancarContas(){
-  debugger
   let Categoria = JSON.parse(localStorage.getItem('Categorias'))
-  if(verificar(Categoria) == true){
+  if(verificar(Categoria)){
     if(localStorage.getItem('Contas') == null){
       localStorage.setItem('Contas',"[]")
       let Contas = JSON.parse(localStorage.getItem('Contas'))
-      let categoria = JSON.parse(localStorage.getItem('Categorias'))
       let id = Date.now();
       let titulo = document.getElementById('txttitulo').value
       let cat = document.getElementById('txtcategoria').value
-
+      
       dados = localizar(cat)
       let tipo = document.getElementById('txttipo').value
                 var CadastroContas = {
@@ -31,7 +29,6 @@ function lancarContas(){
                 localStorage.setItem('Contas',JSON.stringify(Contas))
     }else{
       let Contas = JSON.parse(localStorage.getItem('Contas'))
-      let categoria = JSON.parse(localStorage.getItem('Categorias'))
       let id = Date.now();
       let titulo = document.getElementById('txttitulo').value
       let cat = document.getElementById('txtcategoria').value
@@ -53,51 +50,37 @@ function lancarContas(){
 
   function listarContas(){
     let ContasLancadas = JSON.parse(localStorage.getItem('Contas'))
-    let Categoria = JSON.parse(localStorage.getItem('Categorias'))
     let linha = "";
-
-    if(verificar(Categoria) == true){
-      ContasLancadas.forEach(element => {
-        let row = document.getElementById("id");
-        linha += "<label>"+element.id+"</label><br>"
-        row.innerHTML = linha;
-        });
-    
-        let linhan = "";
-    
-        ContasLancadas.forEach(element => {
-          let row = document.getElementById("titulo");
-          linhan += "<label>"+element.titulo+"</label><br>"
-          row.innerHTML = linhan;
-        });
-
-        let linhacat = "";
-        ContasLancadas.forEach(element => {
-          let row = document.getElementById("categoria");
-          linhacat += "<label>"+element.categoria.nome+"</label><br>"
-          row.innerHTML = linhacat;
-        });
-    
-        let linhatipe = "";
-        ContasLancadas.forEach(element => {
-          let row = document.getElementById("tipo");
-          linhatipe += "<label>"+element.tipo+"</label><br>"
-          row.innerHTML = linhatipe;
-        });
-    
-        let linhati = "";
-        ContasLancadas.forEach(element => {
-          let row = document.getElementById("functions");
-            linhati += "<button class='btn' onclick='EditarConta("+element.id+")'>Editar</button><br>"
-            row.innerHTML = linhati;
-        });
-    }else{
-      alert("Não é possível lançar uma conta sem ter uma categoria lançada. Por favor lance uma categoria")
-      window.location = "cadastrarcategorias.html"
-    }
+    let coluna = ['id','titulo','categoria','tipo','functions']
+    let conteudo = ""
+    let i = 0
+    let indice = 0
+    ContasLancadas.forEach(element => {
+      i = 0
+      while(i < 5){
+        if(coluna[i] === "id"){
+          conteudo = ContasLancadas[indice].id
+        }else if(coluna[i] === "titulo"){
+          conteudo = ContasLancadas[indice].titulo
+        }else if(coluna[i] === "categoria"){
+          conteudo = ContasLancadas[indice].categoria.nome
+        }else if(coluna[i] === "tipo"){
+          conteudo = ContasLancadas[indice].tipo
+        }else{
+          conteudo = "<button class='btn' onclick='EditarConta("+element.id+")'>Editar</button><br>"
+        }
+        let row = document.getElementById(coluna[i]);
+        linha = "<label>"+conteudo+"</label><br>"
+        i++
+        row.innerHTML += linha
+      }
+      
+      indice++
+      });
   }
 
   function EditarConta(id){
+    debugger
     let categorias = JSON.parse(localStorage.getItem('Categorias'))
     let conta = localizarConta(id)
 
@@ -126,15 +109,16 @@ function lancarContas(){
 
     let c = confirm('Alterando essa conta, todas as contas lancadas no painel tambem serão alteradas. Deseja continuar ?')
 
-    if(c == true){
+    if(c){
+      debugger
       ContasLancadas.forEach(element => {
         if(ContasLancadas[index].id == element.id){
           element.titulo = document.getElementById('txtcontatitulo').value;
           var sel = document.getElementById("txtcontacategoria");
           var text= sel.options[sel.selectedIndex].text;
           element.categoria.nome = text;
-          var sel1 = document.getElementById("txtcontatipo");
-          var text1= sel1.options[sel1.selectedIndex].text;
+          var seltipo = document.getElementById("txtcontatipo");
+          var text1= seltipo.options[seltipo.selectedIndex].text;
           element.tipo = text1
           localStorage.setItem('Contas',JSON.stringify(ContasLancadas))
         }
@@ -151,7 +135,7 @@ function lancarContas(){
 
     let c = confirm('Esta conta sera apagada. Continuar ?')
 
-    if(c == true){
+    if(c){
       ContasLancadas.splice(index,1)
       localStorage.setItem('Contas',JSON.stringify(ContasLancadas))
     }else{
@@ -161,7 +145,7 @@ function lancarContas(){
   
 function ListarCatContas(){
     let Categorias = JSON.parse(localStorage.getItem('Categorias'))
-    if(verificar(Categorias) == true){
+    if(verificar(Categorias)){
       let linhaca = "";
       Categorias.forEach(element => {
         let row = document.getElementById("txtcategoria");
